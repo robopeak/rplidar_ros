11829009244b931d7ee7bdd8b491fa3b797f924e
Simon Gene Gottlieb <simon@gottliebtfreitag.de>
2017-04-09 18:05:01 +0200

    fix timeout in initialization

--- sdk/src/arch/linux/net_serial.cpp
+++ sdk/src/arch/linux/net_serial.cpp
@@ -212,6 +212,8 @@ int raw_serial::waitfordata(size_t data_count, _u32 timeout, size_t * returned_s
     timeout_val.tv_sec = timeout / 1000;
     timeout_val.tv_usec = (timeout % 1000) * 1000;
 
+    time_t startTime = time(nullptr) * 1000;
+
     if ( isOpened() )
     {
         if ( ioctl(serial_fd, FIONREAD, returned_size) == -1) return ANS_DEV_ERR;
@@ -223,6 +225,11 @@ int raw_serial::waitfordata(size_t data_count, _u32 timeout, size_t * returned_s
 
     while ( isOpened() )
     {
+        time_t now = time(nullptr) * 1000;
+        if ((now - startTime) > timeout) {
+            return ANS_TIMEOUT;
+        }
+
         /* Do the select */
         int n = ::select(max_fd, &input_set, NULL, NULL, &timeout_val);
 
